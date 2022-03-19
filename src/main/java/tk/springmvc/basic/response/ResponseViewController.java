12759d81f452197c0d2bbdbc1038ce4cd26dca1b
Kim Taekyung <seaking7@gmail.com>
2022-03-19 14:55:52 +0900

    Response처리 여러 방법들

---
+++ src/main/java/tk/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,30 @@
+package tk.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1(){
+        ModelAndView mav = new ModelAndView("response/hello")
+                .addObject("data", "hello!");
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model){
+        model.addAttribute("data", "hello!!!");
+
+        return "response/hello";
+    }
+
+    //권장하지는 않는 방법(RequestMapping 에 있는 url 로 View 찾음)
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model){
+        model.addAttribute("data", "hello!!!?");
+    }
+}
